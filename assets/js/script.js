bba55670ec292c442697efbb087f41f2fc892d7d
Javiera <javierabta97@gmail.com>
2022-04-05 21:00:07 -0400

    Se añadio el footer, tooltips y codigo de js

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+//Tooltip
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+$(function () {
+
+    $('#enviarCorreo').click(function () {
+        alert('El mensaje fue enviado correctamente...');
+    })
+
+    $('.card-text').click(function () {
+        $('.card-body').toggle();
+    });
+
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() < 850) {
+            $("nav").css({ "background-color": "transparent" });
+        }
+        else {
+            $("nav").css({ "background-color": "Black" });
+        }
+
+    })
+
+});
+
